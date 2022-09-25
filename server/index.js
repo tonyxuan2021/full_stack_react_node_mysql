c052139eb6da9053ee1ca9abe91fa31f4d957f3a
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-25 15:32:14 -0700

    complete get the post new employees

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employeeSystem",
+});
+
+// db.connect();
+
+app.post("/create", (req, res) => {
+  const { name, age, country, position, wage } = req.body;
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("values inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(8080, () => {
+  console.log(`Example app listening on port 8080`);
+});
